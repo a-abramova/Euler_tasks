b2761864068c9b3aa8699894da49973156a74056
a-abramova <a.abramova@nv.dunice.net>
2023-02-10 14:44:24 +0300

    resolve task 12

---
+++ task12.js
@@ -0,0 +1,34 @@
+(() => {
+function getTriangleNumber(rightAmount) {
+    let i = 1;
+    let triangleNumber = 0;
+
+    while(true) {
+        triangleNumber += i;
+
+        let count = getDivisorsCount(triangleNumber);
+        if( count > rightAmount) {
+            return triangleNumber;
+        }
+        i++;
+    }
+}
+
+function getDivisorsCount(num) {
+    let counter = 0;
+
+    for(let j = 1; j * j <= num; j++) {
+        if(num % j === 0) {
+            counter += 2;
+        }
+    }
+    return counter;
+}
+
+console.time('answer time');
+const rez = getTriangleNumber(500);
+console.timeEnd('answer time');
+
+// 76576500
+console.log(rez);
+})();
